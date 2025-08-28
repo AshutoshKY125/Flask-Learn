85e5a988ca2c9abd5e7124d951ed4be954390081
AshutoshKY125 <ashutoshkumaryadav040903@gmail.com>
2025-08-29 00:40:16 +0530

    Jinja 2 and Marks % flask

--- html+getpost/getpost.py
+++ html+getpost/getpost.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -8,10 +8,44 @@ def index():
 
 @app.route('/form', methods = ['GET','POST'])
 def form():
-    if request.method == 'POST':
-        college = request.form['college_name']
-        return f'Good for you {college}'
     return render_template('form.html')
+
+@app.route('/submit', methods = ['GET','POST'])
+def submit():
+    if request.method == 'POST':
+        phy_mark = float(request.form['physics'])
+        chem_mark = float(request.form['chemistry'])
+        math_mark = float(request.form['maths'])
+        eng_mark = float(request.form['english'])
+        pe_mark = float(request.form['physical_edu'])
+
+        perc = ((phy_mark+chem_mark+math_mark+eng_mark+pe_mark)/5)
+
+        return redirect (url_for('result', results = perc))
+    
+    
+
+@app.route('/result/<float:results>')
+def result(results):
+    res = "FAILED"
+    Marks_obtained = results
+    if Marks_obtained > 90:
+        res = "Passed with grade S"
+    elif Marks_obtained > 80 and Marks_obtained <= 100:
+        res = "Passes with grade A"
+    elif Marks_obtained > 80 and Marks_obtained <= 90:
+        res = "PASSED with grade B" 
+    elif Marks_obtained > 70 and Marks_obtained <= 80:
+        res = "PASSED with grade C"
+    elif Marks_obtained >= 60 and Marks_obtained <= 100:
+        res = "PASSED with grade D"
+    elif Marks_obtained < 60 :
+        res = "Failed"
+
+    final = {'score':Marks_obtained,'result': res}
+
+    return render_template('result.html', results = final)
+    #return render_template('result.html')
 
 
 if __name__ == "__main__":
